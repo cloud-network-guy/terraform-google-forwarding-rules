3fe779edb8644723895f3539db3dcf6b1ace5c91
cloud-network-guy <greenlakejohnny@gmail.com>
2023-12-23 07:10:30 -0800

    address ouptu

--- outputs.tf
+++ outputs.tf
@@ -5,10 +5,12 @@ output "forwarding_rules" {
       index_key       = v.index_key
       name            = v.name
       region          = v.is_regional ? v.region : "global"
-      address         = v.is_regional ? google_compute_address.default[v.index_key].address : google_compute_global_address.default[v.index_key].address
       id              = v.is_regional ? google_compute_forwarding_rule.default[v.index_key].id : google_compute_global_forwarding_rule.default[v.index_key].id
       self_link       = v.is_regional ? google_compute_forwarding_rule.default[v.index_key].self_link : google_compute_global_forwarding_rule.default[v.index_key].self_link
       backend_service = v.is_regional ? google_compute_forwarding_rule.default[v.index_key].backend_service : google_compute_global_forwarding_rule.default[v.index_key].backend_service
     }
   ]
+}
+output "ip_addresses" {
+  value = [for i, v in local.ip_addresses : (v.is_regional ? google_compute_address.default[v.index_key].address : google_compute_global_address.default[v.index_key].address)]
 }
